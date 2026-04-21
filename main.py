7d7a839927c92b63f8deeaeddeb8e60e26ca2b91
azuari <azuaribobby@gmail.com>
2026-04-21 13:32:58 +0800

    use gunicorn for render

--- main.py
+++ main.py
@@ -76,8 +76,7 @@ def download():
 def get_code():
     return generated_code, 200, {'Content-Type': 'text/plain'}
 
-import os
-
 if __name__ == '__main__':
+    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
